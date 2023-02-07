f61c7e860b68cbac8b9a43c8fcde452738e56b03
Dijcks,Brian B.M <469729@student.fontys.nl>
2023-02-07 13:55:55 +0100

    Database string

--- GroopySwoopyDAL/Databaseconnection.cs
+++ GroopySwoopyDAL/Databaseconnection.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySqlConnector;
 
 namespace GroopySwoopyDAL
 {
     internal static class DatabaseConnection
     {
-        public static string ConnectionString = @"Data Source=(localdb)\Local;Initial Catalog = Leaguepedia; Integrated Security = True";
+        public static MySqlConnection CreateConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(GetConnectionString());
+            connection.Open();
+
+            return connection;
+        }
+
+        private static string GetConnectionString()
+        {
+            //TODO: Should read this from a file
+            string host = "localhost";
+            string username = "rhc_admin";
+            string password = "r00t";
+            string database = "rockstars_health_check";
+
+            //host = "lauren-healthcheck.db.transip.me";
+            //username = "lauren_hcadmin";
+            //password = "r00tpass";
+            //database = "lauren_healthcheck";
+            string cs = $"server={host};userid={username};password={password};database={database}";
+
+
+            //return "Server=studmysql01.fhict.local;Uid=dbi469729;Database=dbi469729;Pwd=test";
+            return cs;
+        }
     }
 }
